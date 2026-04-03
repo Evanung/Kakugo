d09697a2922a100ac25691a9af0a4e734649b533
Evan Ung <evanung7@gmail.com>
2026-04-03 19:32:04 -0400

    Disable prerendering

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -10,7 +10,7 @@ import Aura from '@primeuix/themes/aura';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes), provideClientHydration(withEventReplay()),
+    provideRouter(routes),
     providePrimeNG({
       theme: {
         preset: Aura
